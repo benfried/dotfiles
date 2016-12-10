35bae590715f9e78569a4aac430649d2332f3fff
Ben Fried <ben.fried@gmail.com>
2016-12-10 14:56:08 -0500

    update packages

--- .dotfiles/.emacs.d/elpa/request-20160108.33/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "request" "20160108.33" "Compatible layer for URL request in Emacs" '((cl-lib "0.5")))
--- .dotfiles/.emacs.d/elpa/websocket-20160124.2020/websocket-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "websocket" "20160124.2020" "Emacs WebSocket client and server" 'nil :keywords '("communication" "websocket" "server"))
